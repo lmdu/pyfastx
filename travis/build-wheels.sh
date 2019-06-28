aa00adf4fbdc8d38e32a7f624c5510e5aa8712f3
Lianming Du <adullb@qq.com>
2019-06-28 21:06:37 +0800

    update travis

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -x
 
-yum install -y atlas-devel zlib-devel
+yum install -y zlib-devel
 
 for PYBIN in /opt/python/*/bin; do
 	"${PYBIN}/pip" install nose
@@ -9,10 +9,10 @@ for PYBIN in /opt/python/*/bin; do
 done
 
 for whl in wheelhouse/*.whl; do
-	auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+	auditwheel repair -L . $whl --plat $PLAT -w /io/wheelhouse/
 done
 
-#chown -R --reference=/io/setup.py /io/wheelhouse
+chown -R --reference=/io/setup.py /io/wheelhouse
 
 #for PYBIN in /opt/python/*/bin; do
 #	"${PYBIN}/pip" install pyfastx --no-index -f /io/wheelhouse
