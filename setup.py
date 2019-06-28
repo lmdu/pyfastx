b60b6d59d1ca7ee5daab45f1a2ef20e70af7c88f
Lianming Du <adullb@qq.com>
2019-06-28 20:43:56 +0800

    added travis

--- setup.py
+++ setup.py
@@ -2,30 +2,38 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-#from distutils.core import setup, Extension
-from setuptools import setup, Extension, find_packages
+import glob
+from distutils.core import setup, Extension
+#from setuptools import setup, Extension, find_packages
 
-sources = ['src/module.c', 'src/fasta.c', 'src/index.c', 'src/sequence.c', 'src/kseq.c', 'src/zran.c', 'src/identifier.c', 'src/util.c']
+sources = glob.glob('src/*.c')
 
 extensions = [
 	Extension('pyfastx', sources, extra_link_args=['-lz', '-lsqlite3']),
 ]
 
-description = "pyfastx is a python module for fast random access to FASTA sequences from flat text file or even from gzip compressed file."
+description = """pyfastx is a python module for fast random
+access to FASTA sequences from flat text file or even from
+gzip compressed file."""
+
+with open('README.md') as fh:
+	long_description = fh.read()
+
+with open(os.path.join('src', 'version.h')) as fh:
+	version = fh.read().split()[2].strip('"')
 
 setup(
 	name = 'pyfastx',
-	version = '0.1.0',
+	version = version,
 	ext_modules = extensions,
 	description = description,
-	long_description = description,
+	long_description = long_description,
 	long_description_content_type = "text/markdown",
 	author = 'Lianming Du',
 	author_email = 'adullb@qq.com',
 	url = 'https://github.com/lmdu/pyfastx',
 	license = 'MIT',
 	keywords = 'fasta sequence bioinformatics',
-	#packages = find_packages(),
 	classifiers = [
 		'Development Status :: 3 - Alpha',
 		'Intended Audience :: Developers',
@@ -35,10 +43,10 @@ setup(
 		'License :: OSI Approved :: MIT License',
 		'Programming Language :: C',
 		'Programming Language :: Python :: 3',
-		#'Programming Language :: Python :: 3.4',
-		#'Programming Language :: Python :: 3.5',
-		#'Programming Language :: Python :: 3.6',
-		#'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
 		'Operating System :: Microsoft :: Windows',
 		'Operating System :: POSIX :: Linux',
 		'Operating System :: Unix',
---
+++ travis/build-wheels.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e -x
+
+yum install -y atlas-devel zlib-devel
+
+for PYBIN in /opt/python/*/bin; do
+	"${PYBIN}/pip" install nose
+	"${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+for whl in wheelhouse/*.whl; do
+	auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+done
+
+chown -R --reference=/io/setup.py /io/wheelhouse
+
+#for PYBIN in /opt/python/*/bin; do
+#	"${PYBIN}/pip" install pyfastx --no-index -f /io/wheelhouse
+#	(cd "$HOME";)
+#done
