a851bf508e263b9fb7967e520264a7cf9ba8874d
Lianming Du <adullb@qq.com>
2019-06-28 21:31:30 +0800

    added pwd

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -4,15 +4,14 @@ set -e -x
 yum install -y zlib-devel
 
 for PYBIN in /opt/python/*/bin; do
-	"${PYBIN}/pip" install nose
+	"${PYBIN}/pip" install --upgrade pip wheel
+	"${PYBIN}/pip" install --upgrade setuptools
 	"${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
 for whl in wheelhouse/*.whl; do
-	auditwheel repair -L . $whl --plat $PLAT -w /io/wheelhouse/
+	auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
 done
-
-chown -R --reference=/io/setup.py /io/wheelhouse
 
 #for PYBIN in /opt/python/*/bin; do
 #	"${PYBIN}/pip" install pyfastx --no-index -f /io/wheelhouse
