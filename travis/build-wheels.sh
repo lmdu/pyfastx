d8a03867a7b4f38c359279b98beb4cac8931199a
Lianming Du <adullb@qq.com>
2019-06-28 20:51:09 +0800

    changed build wheel

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -12,7 +12,7 @@ for whl in wheelhouse/*.whl; do
 	auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
 done
 
-chown -R --reference=/io/setup.py /io/wheelhouse
+#chown -R --reference=/io/setup.py /io/wheelhouse
 
 #for PYBIN in /opt/python/*/bin; do
 #	"${PYBIN}/pip" install pyfastx --no-index -f /io/wheelhouse
