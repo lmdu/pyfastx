286d3beebb02e9b2e1965cada02552fac3b5d52f
Lianming Du <adullb@qq.com>
2019-07-01 09:06:44 +0800

    win50

--- ci/fix_compiler_error.py
+++ ci/fix_compiler_error.py
@@ -3,10 +3,12 @@ import sys
 
 filename = os.path.join(sys.argv[1], r'Lib\distutils\cygwinccompiler.py')
 
-print(filename)
-
 with open(filename) as fp:
-	lines = fp.read().replace("            return ['msvcr100']", "            return ['msvcr100']\n        elif msc_ver == '1900':\n            return ['vcruntime140']")
+	if sys.version_info[0:2] > (3, 5):
+		lines = fp.read().replace("            return ['msvcr100']", "            return ['msvcr100']\n        elif msc_ver == '1916':\n            return ['vcruntime140']")
+
+	else:
+		lines = fp.read().replace("            return ['msvcr100']", "            return ['msvcr100']\n        elif msc_ver == '1900':\n            return ['vcruntime140']")
 
 with open(filename, "w") as fw:
 	fw.write(lines)
