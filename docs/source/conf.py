7e055ed844c2d291b70d9548c8b8f542dcfd15f8
Lianming Du <adullb@qq.com>
2019-07-08 20:54:45 +0800

    changed docs2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,6 @@ import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-master_doc = 'index'
-
 # -- Project information -----------------------------------------------------
 
 project = 'pyfastx'
