3a3c759bbbfc8fc91d13241642e06d270dffa0e3
Lianming Du <adullb@qq.com>
2019-07-15 07:32:32 +0800

    added test

--- test/test.py
+++ test/test.py
@@ -1,58 +1,102 @@
 import os
+import random
 import pyfastx
-import sqlite3
+import pyfaidx
 import unittest
 
 os.chdir(os.path.dirname(__file__))
 
-fasta_file = 'data/test.fa.gz'
-fasta_index = 'data/test.fa.gz.db'
+gzip_fasta = 'data/test.fa.gz'
+flat_fasta = 'data/test.fa'
 
 class FastaTest(unittest.TestCase):
 	def setUp(self):
-		self.fasta = pyfastx.Fasta(fasta_file)
-		self.conn = sqlite3.connect(fasta_index)
+		self.fastx = pyfastx.Fasta(gzip_fasta)
+		self.faidx = pyfaidx.Fasta(flat_fasta, sequence_always_upper=True)
+		self.count = len(self.faidx)
 
 	def tearDown(self):
-		try:
+		if os.path.exists('data/test.fa.gz.db'):
 			os.remove('data/test.fa.gz.db')
-		except EnvironmentError:
-			pass
 
-	def expect_val(self, sql):
-		cursor = self.conn.cursor()
-		cursor.execute(sql)
-		row = cursor.fetchone()
-		cursor.close()
-		if len(row) == 1:
-			return row[0]
-		else:
-			return row
+		if os.path.exists('data/test.fa.fai'):
+			os.remove('data/test.fa.fai')
 
-	def test_build_index(self):
-		self.assertTrue(os.path.exists(fasta_index))
+	def get_random_index(self):
+		return random.randint(0, self.count-1)
 
-	def test_fasta_attrs(self):
-		fasta = self.fasta
+	def test_fasta(self):
+		#seq counts
+		self.assertEqual(len(self.fastx), len(self.faidx.keys()))
 
-		sql = "SELECT COUNT(*) FROM seq LIMIT 1"
-		self.assertEqual(expect_val(sql), fasta.count)
+		#seq length
+		expect = sum(len(s) for s in self.faidx)
+		self.assertEqual(self.fastx.size, expect)
 
-		sql = "SELECT SUM(slen) FROM seq LIMIT 1"
-		self.assertEqual(expect_val(sql), fasta.size)
+	def test_keys(self):
+		expect = list(self.faidx.keys())
+		result = list(self.fastx.keys())
 
-		self.assertEqual(fasta_file, fasta.file_name)
+		self.assertEqual(expect, result)
+
+	def test_seq_by_index(self):
+		#test get seq by index
+		idx = self.get_random_index()
+		expect = self.faidx[idx][:]
+		result = self.fastx[idx]
+
+		self.assertEqual(expect.name, result.name)
+		self.assertEqual(expect.seq, result.seq)
+
+		#test negative index
+		idx = (self.get_random_index() + 1) * -1
+		expect = self.faidx[idx][:]
+		result = self.fastx[idx]
+
+		self.assertEqual(expect.name, result.name)
+		self.assertEqual(expect.seq, result.seq)
+
+	def test_seq_by_key(self):
+		idx = self.get_random_index()
+		key = list(self.faidx.keys())[idx]
+
+		expect = self.faidx[key][:]
+		result = self.fastx[key]
+
+		self.assertEqual(expect.name, result.name)
+		self.assertEqual(expect.name, result.seq)
+
+	def test_seq_reverse_complement(self):
+		idx = self.get_random_index()
+		expect = self.faidx[idx][:]
+		result = self.fastx[idx]
+
+		self.assertEqual(expect.reverse.seq, result.reverse)
+		self.assertEqual(expect.complement.seq, result.complement)
+		self.assertEqual(-expect, result.antisense)
+
+	def test_seq_slice(self):
+		idx = self.get_random_index()
+		expect = self.faidx[idx]
+		result = self.fastx[idx]
+
+		self.assertEqual(expect[5:10].seq, result[5:10].seq)
+		self.assertEqual(expect[20:].seq, result[20:].seq)
+		self.assertEqual(expect[-10:-1].seq, result[-10:-1].seq)
 
 	def test_get_seq(self):
-		fasta = self.fasta
+		idx = self.get_random_index()
+		expect = self.faidx[idx]
+		result = self.fastx[idx]
 
-		seq = fasta[0]
+		
 
-		sql = "SELECT * FROM seq WHERE id=0 LIMIT 1"
-		row = expect_val(sql)
 
-		self.assertEqual(seq.name, row[1])
-		self.assertEqual(len(seq.seq), row[4])
+
+
+
+
+
 
 
 
