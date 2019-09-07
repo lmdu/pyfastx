17e7443f3ece2cbcf389c8147c4e7de8c95728d6
Lianming Du <adullb@qq.com>
2019-09-07 22:31:32 +0800

    fixed description unicode string

--- src/sequence.c
+++ src/sequence.c
@@ -60,14 +60,18 @@ PyObject *pyfastx_sequence_get_name(pyfastx_Sequence* self, void* closure){
 
 PyObject *pyfastx_sequence_description(pyfastx_Sequence* self, void* closure){
 	sqlite3_stmt *stmt;
+	Py_ssize_t nbytes;
+	char *descript;
+
 	const char *sql = "SELECT descript FROM seq WHERE seqid=? LIMIT 1";
 	sqlite3_prepare_v2(self->index->index_db, sql, -1, &stmt, NULL);
 	sqlite3_bind_text(stmt, 1, self->name, -1, NULL);
-	
+
 	if (sqlite3_step(stmt) == SQLITE_ROW) {
-		char *header = (char *)sqlite3_column_text(stmt, 0);
+		descript = (char *)sqlite3_column_text(stmt, 0);
+		nbytes = sqlite3_column_bytes(stmt, 0);
 		sqlite3_finalize(stmt);
-		return Py_BuildValue("s", header);
+		return PyUnicode_FromStringAndSize(descript, nbytes);
 	}
 
 	Py_RETURN_NONE;
