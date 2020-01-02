e48ad063541f30e3581f809a021a23b2d71442e3
lmdu <adullb@qq.com>
2020-01-02 22:43:11 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
             "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8"
+            "Programming Language :: Python :: 3.8",
             "Operating System :: Microsoft :: Windows",
             "Operating System :: POSIX :: Linux",
             "Operating System :: Unix",
